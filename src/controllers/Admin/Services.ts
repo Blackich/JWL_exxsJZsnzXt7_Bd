63ad898bdb70d482c099ad49c1c1674316ffff6c
OnlyBlack <aera.onlyblack@gmail.com>
2024-11-24 20:30:51 +0500

    refactor:  modify the query for the purchase table

--- src/controllers/Admin/Services.ts
+++ src/controllers/Admin/Services.ts
@@ -5,16 +5,17 @@ import { dbError, tryCatch } from "@src/middleware/errorHandler";
 export const getUsersPurchasedServices = tryCatch(
   async (req: Request, res: Response) => {
     db.query(
-      `SELECT s.id, s.userId, sn.nickname, p.likes, 
-        s.customPackage, s.orderId, s.countPosts, 
-        s.status, s.createdAt, s.cost,
-        s.currency, e.fullName
-        FROM Service s, Social_nickname sn,
-        Package p, Employees e, Users u
-        WHERE s.socialNicknameId = sn.id
-          AND s.packageId = p.id
-          AND s.userId = u.id
-          AND u.invitedEmployeeId = e.id`,
+      `SELECT s.id, s.userId, em.fullName, sn.nickname, 
+		    s.packageId, p.likes as packageLikes,
+        s.customPackageId, cp.likes as customLikes,
+		    s.countPosts, s.orderId, s.status, s.createdAt,
+        s.cost, s.currency
+          FROM Service s
+          LEFT JOIN Package p ON p.id = s.packageId
+          LEFT JOIN Custom_package cp ON cp.id = s.customPackageId
+          LEFT JOIN Social_nickname sn ON sn.id = s.socialNicknameId
+          LEFT JOIN Users u ON u.id = s.userId
+          LEFT JOIN Employees em ON em.id = u.invitedEmployeeId`,
       (err, result) => {
         if (err) return dbError(err, res);
         const data = result;
