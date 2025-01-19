4b358c2dfe5b49be37d88ec07fd83bb19b05d95c
Blackich <aera.onlyblack@gmail.com>
2025-01-19 18:51:31 +0500

    refactor: new error handling

--- src/utils/intermediateReq.ts
+++ src/utils/intermediateReq.ts
@@ -1,5 +1,5 @@
 import { db } from "@src/main";
-import { logger } from "./logger/logger";
+import { logErr } from "@src/middleware/errorHandler";
 import {
   CustomPackageDetails,
   PackageDetails,
@@ -9,77 +9,49 @@ import {
 } from "./types";
 
 export const getPackageDetailsById = async (id: number) => {
-  const data = await db
+  return await db
     .promise()
     .query(`SELECT * FROM Package_detail WHERE id = ${id}`)
-    .then(([result]) => {
-      return (result as PackageDetails[])[0];
-    })
-    .catch((err) => logger.error(err.stack));
-  return data;
+    .then(([result]) => (result as PackageDetails[])[0])
+    .catch((err) => logErr(err, "intReq/getPackageDetailsById"));
 };
 
 export const getSocialNicknameById = async (id: number) => {
-  const data = await db
+  return await db
     .promise()
     .query(`SELECT * FROM Social_nickname WHERE id = ${id}`)
-    .then(([result]) => {
-      return (result as SocialNickname[])[0].nickname;
-    })
-    .catch((err) => logger.error(err.stack));
-  return data;
+    .then(([result]) => (result as SocialNickname[])[0]?.nickname)
+    .catch((err) => logErr(err, "intReq/getSocialNicknameById"));
 };
 
 export const getCustomPackageDetailsById = async (id: number) => {
-  const data = await db
+  return await db
     .promise()
     .query(`SELECT * FROM Custom_package_detail WHERE id = ${id}`)
-    .then(([result]) => {
-      return (result as CustomPackageDetails[])[0];
-    })
-    .catch((err) => logger.error(err.stack));
-  return data;
+    .then(([result]) => (result as CustomPackageDetails[])[0])
+    .catch((err) => logErr(err, "intReq/getCustomPackageDetailsById"));
 };
 
 export const packageSettings = async () => {
-  const data = await db
+  return await db
     .promise()
     .query(
       `SELECT * FROM Package_setting
         WHERE status = 1`,
     )
-    .then(([result]) => {
-      return result as PackageSettings[];
-    })
-    .catch((err) => logger.error(err.stack));
-  return data;
+    .then(([result]) => result as PackageSettings[])
+    .catch((err) => logErr(err, "intReq/packageSettings"));
 };
 
 export const getExtraServiceNameByExtraId = async (extraServiceId: number) => {
-  const data = await db
+  return await db
     .promise()
     .query(
       `SELECT serviceName FROM Extra_service
         WHERE id = ${extraServiceId}`,
     )
-    .then(([result]) => {
-      return (result as { serviceName: string }[])[0].serviceName;
-    })
-    .catch((err) => logger.error(err.stack));
-  return data;
-};
-
-export const getSiteResNameById = async (id: number) => {
-  return await db
-    .promise()
-    .query(
-      `SELECT siteName FROM Site_res
-        WHERE id = ${id}`,
-    )
-    .then(([result]) => {
-      return (result as { siteName: string }[])[0].siteName;
-    })
-    .catch((err) => logger.error(err.stack));
+    .then(([result]) => (result as { serviceName: string }[])[0]?.serviceName)
+    .catch((err) => logErr(err, "intReq/getExtraServiceNameByExtraId"));
 };
 
 export const getExchangeRate = async () => {
@@ -89,9 +61,6 @@ export const getExchangeRate = async () => {
       `SELECT * FROM Exchange_rate
         WHERE typeRate = 'external'`,
     )
-    .then(([result]) => {
-      const data = Number((result as ExchangeRate[])[0].value);
-      return data;
-    })
-    .catch((err) => logger.error(err.stack));
+    .then(([result]) => Number((result as ExchangeRate[])[0]?.value))
+    .catch((err) => logErr(err, "intReq/getExchangeRate"));
 };
